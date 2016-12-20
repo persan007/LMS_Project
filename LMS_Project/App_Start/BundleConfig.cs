59dffdcd4550b67afbb9bc29a5c03f540c165deb
Kristoffer <krillllind@gmail.com>
2016-12-20 13:39:21 +0100

    Added SCSS files

--- LMS_Project/App_Start/BundleConfig.cs
+++ LMS_Project/App_Start/BundleConfig.cs
@@ -9,40 +9,55 @@ namespace LMS_Project
 
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // Load jQuery
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"
-            ));
+            //// Load jQuery
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //    "~/Scripts/jquery-{version}.js"
+            //));
 
-            // Load jQuery Validate
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"
-            ));
+            //// Load jQuery Validate
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //    "~/Scripts/jquery.validate*"
+            //));
 
             // Load Modernizer
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-*"
             ));
 
-            // Load Bootstrap
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"
+            //// Load Bootstrap
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //    "~/Scripts/bootstrap.js",
+            //    "~/Scripts/respond.js"
+            //));
+
+            //// Load Angular with custom controllers
+            //bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+            //    "~/Scripts/angular.js",
+            //    "~/LMS/*.js",
+            //    "~/LMS/Filters/*.js",
+            //    "~/LMS/Providers/*.js",
+            //    "~/LMS/Controllers/*.js"
+            //));
+
+            // Load Angular min file using GruntJS //
+            bundles.Add(new ScriptBundle("~/bundles/core").Include(
+                "~/project.core.min.js"
             ));
 
-            // Load Angular with custom controllers
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/angular.js",
-                "~/LMS/*.js",
-                "~/LMS/Filters/*.js",
-                "~/LMS/Providers/*.js",
-                "~/LMS/Controllers/*.js"
+            // Load jQuery and Bootstrap min file using GruntJS //
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                "~/project.vendor.min.js"
             ));
+
+            //// Load page stylesheets
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //    "~/Content/bootstrap.css",
+            //    "~/Content/site.css"
+            //));
 
             // Load page stylesheets
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"
+                "~/style.css"
             ));
         }
     }
---
+++ LMS_Project/gruntfile.js
@@ -0,0 +1,29 @@
+﻿/*global module */
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: { "style.css": "sass/style.scss" }
+            }
+        },
+        uglify: {
+            core: { files: { 'project.core.min.js': ['Scripts/angular.js', 'LMS/*.js', 'LMS/**/*.js'] } },
+            vendor: { files: { 'project.vendor.min.js': ['Scripts/jquery-1.10.2.min.js', 'Scripts/bootstrap.js'] } }
+        },
+        watch: {
+            assets: { files: ['Sass/**/*.scss'], tasks: ['sass'] },
+            scripts: { files: ['Scripts/**/*.js', 'LMS/**/*.js'], tasks: ['uglify'] }
+        }
+    });
+
+    grunt.registerTask('watch', ['watch']);
+
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-sass");
+};
