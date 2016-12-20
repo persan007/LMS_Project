2f9b63d404f11d13ef949d9053c756587120f5bf
Kristoffer <krillllind@gmail.com>
2016-12-20 15:06:40 +0100

    Login from angular

---
+++ LMS_Project/LMS/Controllers/LoginController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    var LoginController = function ($scope, Request) {
+
+        var Authenticate = function () {
+            Request.Make("TO", $scope.Credentials).then(function (data) {
+                // TODO: när användaren trycker logga in
+            });
+        }
+
+        $scope.Authenticate = Authenticate;
+        $scope.Credentials = {
+            Username: "",
+            Password: ""
+        };
+    }
+
+    LMSApp.controller("LoginController", [
+        "$scope",
+        "Request",
+        LoginController
+    ]);
+
+}());
---
+++ LMS_Project/LMS/Providers/RequestProvider.js
@@ -0,0 +1,38 @@
+﻿(function () {
+
+    var Request = function ($http) {
+        var status = null;
+
+        // If an error occurs display it in the console
+        var OnError = function (response) {
+            console.error(response);
+            status = response.status;
+            return response;
+        }
+
+        // Returns the data obj from server
+        var make = function (TO, DATA) {
+            TO = TO || null;
+
+            if (!TO) {
+                return TO;
+            }
+
+            return $http.post(TO, DATA).then(function (response) {
+                status = response.status;
+                return response.data;
+            }, OnError);
+        }
+
+        return {
+            Make: make,
+            Status: status
+        };
+    }
+
+    LMSApp.provider("Request", [
+        "$http",
+        Request
+    ]);
+
+}());
