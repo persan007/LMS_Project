71d33377797d73b62f79ca699bd69ddacaaea56f
phixle- <henry.ohman@yahoo.se>
2016-12-22 09:45:08 +0100

    Nice login page

--- LMS_Project/Controllers/HomeController.cs
+++ LMS_Project/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace LMS_Project.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
--- LMS_Project/Models/IdentityModels.cs
+++ LMS_Project/Models/IdentityModels.cs
@@ -10,8 +10,6 @@ namespace LMS_Project.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public virtual ICollection<Test> Testies { get; set; }
-        
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -27,8 +25,6 @@ namespace LMS_Project.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
-        public DbSet<Test> Testies { get; set; }
 
         public static ApplicationDbContext Create()
         {
