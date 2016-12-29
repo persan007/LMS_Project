c424ec94a0bf540001d12475d71d7cc6f926a323
phixle- <henry.ohman@yahoo.se>
2016-12-29 09:22:42 +0100

    Added Repositoryies/Repository.cs Moved code from HomeController to Repository

--- LMS_Project/Controllers/HomeController.cs
+++ LMS_Project/Controllers/HomeController.cs
@@ -9,13 +9,14 @@ using System.Web.Mvc;
 using Newtonsoft.Json;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using LMS_Project.Repositories;
 
 namespace LMS_Project.Controllers
 {
     //[Authorize]
     public class HomeController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private Repository _repo = new Repository();
 
         public ActionResult Index()
         {
@@ -27,98 +28,26 @@ namespace LMS_Project.Controllers
             return View();
         }
 
+        public File DownloadFile(string fileName)
+        {
+            return _repo.DownloadFile(fileName);
+        }
+
+        public string GetUrlByFilename(string fileName)
+        {
+            return _repo.GetUrlByFilename(fileName);
+        }
+
         public string[] GetAllFilenames()
         {
-            List<FileObjectModels> files = db.FilesObjects.ToList();
-            string[] filenames = new string[files.Count()];
-            int index = 0;
-            foreach(var file in files)
-            {
-                filenames[index] = file.Filename;
-                index++;
-            }
-            return filenames;
-        }
-
-        public string GetUrlByFilename(string fileName = "ppap.png")
-        {
-            FileObjectModels file = db.FilesObjects.Single(f => f.Filename == fileName);
-
-            if (file != null)
-                return string.Format("data:{0};base64,{1}", file.ContentType, Convert.ToBase64String(file.Data));
-
-            return null; 
-        }
-
-        public string DeBlobber(byte[] bytes)
-        {
-            return Encoding.UTF8.GetString(bytes);
-        }
-
-        public byte[] FileToBlob(string filepath)
-        {
-            //A stream of bytes that represents the binary file
-            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
-
-            //The reader reads the binary data from the file stream
-            BinaryReader reader = new BinaryReader(fs);
-
-            //Bytes from the binary reader stored in BlobValue array
-            byte[] BlobValue = reader.ReadBytes((int)fs.Length);
-
-            //Close stream
-            fs.Close();
-            //Close reader
-            reader.Close();
-
-            return BlobValue;
-        }
-
-        //[HttpPost]
-        public ActionResult DownloadFiles(string fileName = "ppap.png")
-        {
-            //Retrieve file with corrisponding filename
-            var file = db.FilesObjects.Where(f => f.Filename == fileName).First();
-            //Get file blob data
-            var blob = file.Data;
-            //Return generated file
-            return File(blob, file.ContentType, file.Filename);
+            return _repo.GetAllFilenames();
         }
 
         [HttpPost]
-        public void UploadFiles()
+        public ActionResult UploadFiles()
         {
-            for(int i = 0; i < Request.Files.Count; i++)
-            {
-                //Get file, filename and path
-                var file = Request.Files[i];
-                string fileName = Path.GetFileName(file.FileName);
-                string path = Path.Combine(Server.MapPath("~/Resources/Tmp/"), fileName);
-                file.SaveAs(path);
-
-                //Get Blob value from file via filepath
-                byte[] bytes = FileToBlob(path);
-
-                //Create relevant model
-                FileObjectModels tmpModel = new FileObjectModels();
-                tmpModel.Filename = fileName;
-                tmpModel.Data = bytes;
-                tmpModel.ContentType = file.ContentType;
-
-                //Check if model already exists
-                bool dataExists = db.FilesObjects.Any(m => m.Data.Equals(bytes));
-                if(!dataExists)
-                {
-                    db.FilesObjects.Add(tmpModel);
-                    db.SaveChanges();
-                }
-                //Get path to file
-                string fullPath = Request.MapPath("~/Resources/Tmp/" + fileName);
-
-                //Check if file exists, if it does delete it
-                if(System.IO.File.Exists(fullPath))
-                    System.IO.File.Delete(fullPath);
-            }
+            _repo.UploadFiles(Request.Files, Server.MapPath("~/Resources/Tmp/"));
+            return View();
         }
 
         public string GetUserInformation()
---
+++ LMS_Project/Repositories/Repository.cs
@@ -0,0 +1,171 @@
+﻿using LMS_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS_Project.Repositories
+{
+    public class Repository
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// Returns the filenames of all files currently in the database
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetAllFilenames()
+        {
+            List<FileObjectModels> files = db.FilesObjects.ToList();
+            string[] filenames = new string[files.Count()];
+            int index = 0;
+            foreach (var file in files)
+            {
+                filenames[index] = file.Filename;
+                index++;
+            }
+            return filenames;
+        }
+
+        /// <summary>
+        /// Returns a URL to the file with corresponding filename
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public string GetUrlByFilename(string fileName = "ppap.png")
+        {
+            FileObjectModels file = db.FilesObjects.Single(f => f.Filename == fileName);
+            if (file != null)
+                return string.Format("data:{0};base64,{1}", file.ContentType, Convert.ToBase64String(file.Data));
+            return null;
+        }
+
+        /// <summary>
+        /// Converts byte[] bytes to a readable string
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public string DeBlobber(byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        /// <summary>
+        /// Generate blob from file and return said value
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        public byte[] FileToBlob(string filepath)
+        {
+            //A stream of bytes that represents the binary file
+            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+
+            //The reader reads the binary data from the file stream
+            BinaryReader reader = new BinaryReader(fs);
+
+            //Bytes from the binary reader stored in BlobValue array
+            byte[] BlobValue = reader.ReadBytes((int)fs.Length);
+
+            //Close stream
+            fs.Close();
+            //Close reader
+            reader.Close();
+
+            //Return Blob
+            return BlobValue;
+        }
+
+        /// <summary>
+        /// Returns a generated file based on filename
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public File DownloadFile(string fileName = "ppap.png")
+        {
+            //Retrieve file with corrisponding filename
+            var file = db.FilesObjects.Where(f => f.Filename == fileName).First();
+            //Return file
+            return Controller.File(file.Data, file.ContentType, file.Filename);
+        }
+
+        /// <summary>
+        /// Convert file to blob, generate a FileObjectmodel and add to database
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="mapPath"></param>
+        public void UploadFiles(HttpFileCollectionBase files, string mapPath)
+        {
+            for (int i = 0; i < files.Count; i++)
+            {
+                //Get file, filename and path
+                var file = files[i];
+                string fileName = Path.GetFileName(file.FileName);
+                string path = Path.Combine(mapPath, fileName);
+                file.SaveAs(path);
+
+                //Get Blob value from file via filepath
+                byte[] bytes = FileToBlob(path);
+
+                //Check if file exists in database
+                DataExists(bytes, fileName, file.ContentType);
+                
+                //If there is a file in the temp folder, remove it
+                DeleteFile(mapPath + fileName);
+            }
+        }
+
+        /// <summary>
+        /// Creates and returns a new FileObjectModel based on input
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="bytes"></param>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
+        private FileObjectModels CreateModel(string fileName, byte[] bytes, string contentType)
+        {
+            //Create relevant model
+            FileObjectModels model  = new FileObjectModels();
+            model.Filename          = fileName;
+            model.Data              = bytes;
+            model.ContentType       = contentType;
+            return model;
+        }
+
+        /// <summary>
+        /// Check and Delete file based on filepath
+        /// </summary>
+        /// <param name="fullPath"></param>
+        private bool DeleteFile(string filepath)
+        {
+            //Check if file exists, if it does delete it
+            if (System.IO.File.Exists(filepath))
+            {
+                System.IO.File.Delete(filepath);
+                return true;
+            }
+            return false;  
+        }
+
+        /// <summary>
+        /// Check if file already exists in database. If not, add to database
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="fileName"></param>
+        /// <param name="contentType"></param>
+        private bool DataExists(byte[] bytes, string fileName, string contentType)
+        {
+            //Check if model already exists
+            bool dataExists = db.FilesObjects.Any(m => m.Data.Equals(bytes));
+            if (!dataExists)
+            {
+                db.FilesObjects.Add(CreateModel(fileName, bytes, contentType));
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
