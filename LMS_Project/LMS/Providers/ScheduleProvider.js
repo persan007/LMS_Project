17d5f3e088ce0b61a1ab34b142bd8f2b2d3db12f
Kristoffer <krillllind@gmail.com>
2016-12-30 15:48:53 +0100

    Working on schedule provider

--- LMS_Project/LMS/Providers/ScheduleProvider.js
+++ LMS_Project/LMS/Providers/ScheduleProvider.js
@@ -1,47 +1,85 @@
 ﻿(function () {
-    var ScheduleProvider = function ($http) {
-        var status = null;
+    var ScheduleProvider = function ($interval) {
+        var titles = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-        // If an error occurs display it in the console
-        var OnError = function (response) {
-            console.error("Error: " + response);
-            status = response.status;
-            return response;
+        var canvas = document.getElementById("ScheduleCanvas");
+        var ctx = canvas.getContext("2d");
+
+        var numberOfColumns,
+            dayStartsAtSunday,
+            dayStartsAt,
+            dayEndsAt;
+
+        // Used for clearing the canvas //
+        var clear = function () {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
 
-        // Returns all lessons in the database
-        //var allLessons = function () {
-        //    return $http.get('/Home/GetAllLessons')
-        //        .then(function (response) {
-        //            $scope.lessons = respone.data;
-        //        }, OnError);
-        //}
+        var setSize = function () {
+            ctx.canvas.width = canvas.clientWidth;
+            ctx.canvas.height = canvas.clientHeight;
 
-        var make = function (TO, DATA) {
-            TO = TO || null;
+            // Clear the canvas //
+            clear();
 
-            console.log("TO: " + TO);
-            console.log("DATA: " + DATA);
+            // Call draw method to sync //
+            draw();
+        }
 
-            if (!TO) {
-                return TO;
-            }
+        // Draw all the data to the canvas //
+        var draw = function () {
 
-            return $http.post(TO, DATA).then(function (response) {
-                status = response.status;
-                return response.data;
-            }, OnError);
+            // Draw board //
+            (function Board() {
+                ctx.lineWidth = 1;
+                ctx.strokeStyle = "#000000";
+
+                var columnWidth = canvas.width / numberOfColumns;
+
+                for (var i = 0; i < numberOfColumns; i++) {
+                    ctx.moveTo(columnWidth * i, 0);
+                    ctx.lineTo(columnWidth * i, canvas.height);
+                }
+
+                ctx.stroke();
+            })();
+
+            // Draw headers //
+            (function Headers() {
+                ctx.lineWidth = 1;
+                ctx.strokeStyle = "#000000";
+
+                var height = canvas.height / 16;
+
+                ctx.moveTo(0, height);
+                ctx.lineTo(canvas.width, height);
+
+                ctx.stroke();
+            }());
+        }
+
+        // Initialize the canvas settings //
+        var init = function (boardColumns, startAtSunday, dayStarts, dayEnds) {
+            numberOfColumns = boardColumns;
+            dayStartsAtSunday = ((startAtSunday === true) ? true : false);
+            dayStartsAt = dayStarts;
+            dayEndsAt = dayEnds;
+
+            // Add eventlistener for canvas resize //
+            $interval(setSize, 500);
         }
 
         return {
-            //AllLessons: allLessons,
-            Make: make,
-            Status: status
+            Initialize: init,
+            UpdateCanvasSize: setSize,
+            Draw: draw,
+            Clear: clear,
+            element: canvas
         };
     }
 
-    LMSApp.factory("ScheduleProvider", [
-        "$http",
+    LMSApp.factory("Schedule", [
+        "$interval",
         ScheduleProvider
     ]);
 
