6958efbed325b520f58fb57f8199f4ed4357985f
phixle- <henry.ohman@yahoo.se>
2016-12-30 13:13:27 +0100

    ImageDirective

---
+++ LMS_Project/LMS/Directives/ImageDirective.js
@@ -0,0 +1,25 @@
+﻿(function () {
+
+    var image = function () {
+
+        var bind = function (scope, element, attributes) {
+            scope.$watch("bgImage", function (url) {
+                element.css({
+                    'background-image': 'url(' + url + ')',
+                    'background-size': 'cover'
+                });
+            });
+        };
+
+        return {
+            scope: {
+                bgImage: "="
+            },
+            link: bind
+        };
+
+    }
+
+    LMSApp.directive("bgImage", [image]);
+
+}());
