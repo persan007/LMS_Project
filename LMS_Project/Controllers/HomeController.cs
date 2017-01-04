43107233d047c8334eeed54a2b0c11a549555ef1
Kristoffer Lindström <krillllind@gmail.com>
2017-01-04 23:27:47 +0100

    Moved data handling to new controller

--- LMS_Project/Controllers/HomeController.cs
+++ LMS_Project/Controllers/HomeController.cs
@@ -1,129 +1,16 @@
-﻿using LMS_Project.Models;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
-using System.Data.Entity;
-using Newtonsoft.Json;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using LMS_Project.Repositories;
-using System.Web.Security;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity.EntityFramework;
-using LMS_Project.Filters;
 
 namespace LMS_Project.Controllers
-{
-    [Authorize]
+{ 
     public class HomeController : Controller
     {
-        private Repository _repo = new Repository();
-
+        [HttpGet]
+        [Authorize]
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult FileUpload()
-        {
-            return View();
-        }
-
-        public ActionResult DownloadFile(string fileName)
-        {
-            //Retrieve file with corrisponding filename
-            var file = _repo.db.FilesObjects.Where(f => f.Filename == fileName).First();
-            //Return file
-            return File(file.Data, file.ContentType, file.Filename);
-        }
-
-        public string GetUrlByFilename(string fileName)
-        {
-            return _repo.GetUrlByFilename(fileName);
-        }
-
-        public string[] GetAllFilenames()
-        {
-            return _repo.GetAllFilenames();
-        }
-
-        public string GetAllLessons()
-        {
-            LessonModels[] arr = _repo.GetAllLessons();
-            return JsonConvert.SerializeObject(arr, Formatting.None, new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-        }
-
-        [HttpPost]
-        public ActionResult UploadFiles()
-        {
-            _repo.UploadFiles(Request.Files, Server.MapPath("~/Resources/Tmp/"));
-            return View();
-        }
-
-        public string GetAllRoleNames()
-        {
-            var roles = _repo.GetAllRoleNames();
-            return JsonConvert.SerializeObject(roles, Formatting.None, new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects }); ;
-        }
-
-        public async Task<string> GetUserInformation()
-        {
-            var db = new ApplicationDbContext();
-            var User_id = User.Identity.GetUserId();
-            var rolesForUser = await System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().GetRolesAsync(User_id);
-
-            var CurrentUser = db.Users.Select(o => new
-            {
-                Id = o.Id,
-                Firstname = o.Firstname,
-                Lastname = o.Lastname,
-                ProfileImage = o.ProfileImage ?? "http://placehold.it/100x100",
-                Role = rolesForUser.ToList().FirstOrDefault()
-            }).Where(o => o.Id == User_id);
-
-            return JsonConvert.SerializeObject(CurrentUser, Formatting.None, new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-        }
-
-        [HttpGet]
-        [ValidateAngularAntiForgery]
-        public string GetSchedule()
-        {
-            List<object> test = new List<object>() {
-                new {
-                    From = "10:30",
-                    To = "12:00",
-                    Day = "Tuesday",
-                    LessonType = "English",
-                    Color = "lightblue",
-                    Teacher = "TLUG",
-                    Classroom = "C320"
-                },
-                new {
-                    From = "10:30",
-                    To = "15:20",
-                    Day = "Friday",
-                    LessonType = "Programming",
-                    Color = "pink",
-                    Teacher = "ELÖV",
-                    Classroom = "D220"
-                },
-                new {
-                    From = "08:30",
-                    To = "09:45",
-                    Day = "Monday",
-                    LessonType = "Math",
-                    Color = "lightgreen",
-                    Teacher = "POLV",
-                    Classroom = "A332"
-                }
-            };
-
-            return JsonConvert.SerializeObject(test, Formatting.None, new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
         }
     }
 }
