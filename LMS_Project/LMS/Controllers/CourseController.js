8ef8b45345b5defccbe1eda471669ce4aca2a030
Kristoffer <krillllind@gmail.com>
2017-01-04 15:06:55 +0100

    Adding controller for courses page

---
+++ LMS_Project/LMS/Controllers/CourseController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    var CourseController = function ($scope, $routeParams, Request) {
+        
+        //Request.Make("/Home/GetCourses", "get", $routeParams.courseName || null).then(function (data) {
+        //    console.log(data);
+        //});
+    }
+
+    LMSApp.controller("CourseController", [
+        "$scope",
+        "$routeParams",
+        "Request",
+        CourseController
+    ]);
+
+})();
