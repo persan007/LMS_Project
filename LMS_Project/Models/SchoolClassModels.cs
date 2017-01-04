8c71ae9246d5ec887103683c869a91c1a1690695
persan007 <per-ake.sandstrom@hotmail.se>
2017-01-04 08:57:38 +0100

    sdsds

---
+++ LMS_Project/Models/SchoolClassModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LMS_Project.Models
+{
+    public class SchoolClassModels
+    {
+        [Key]
+        public string SchoolClassID { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<ApplicationUser> Students { get; set; }
+
+        //[ForeignKey("SchemaModels")]
+        public string SchemaID { get; set; }
+
+    }
+
+    
+}
