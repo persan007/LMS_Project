399f507210bab2bc2bbcb4b5007f43a448fce2f6
phixle- <henry.ohman@yahoo.se>
2017-01-04 16:05:27 +0100

    Simple error reporting

--- LMS_Project/LMS/Controllers/UserController.js
+++ LMS_Project/LMS/Controllers/UserController.js
@@ -5,34 +5,49 @@
             if (isValid) {
                 alert('our form is amazing');
                 console.log($scope.user);
-                Request.Make("/Account/GetAntiForgeryToken/", "post").then(function (data) {
-                    Request.Make("/Account/Register/", "post", JSON.stringify($scope.user), null, { 'RequestVerificationToken': data });
+                Request.Make("/Account/GetAntiForgeryToken/", "post").then(function (token) {
+                    Request.Make("/Account/Register/", "post", JSON.stringify($scope.user), null, { 'RequestVerificationToken': token }).then(function (info) {
+                        if (Request.Status != 200) {
+                            console.log("Error: " + info);
+                        }
+                        else {
+                            console.log("All good");
+                        }
+                    });
                 });
             }
             else
             {
                 alert('The form is missing some stuff');
             }
-            
+
+             //DummyUser
+             //Request.Make("/Account/GetAntiForgeryToken/", "post").then(function (data) {
+             //    Request.Make("/Account/Register/", "post", JSON.stringify(DummyUser), null, { 'RequestVerificationToken': data }).then(function (data) {
+             //        console.log(data);
+             //    });
+             //});
         }
 
-        Request.Make("/Home/GetAllRoleNames", "get").then(function (data) {
-            $scope.user.UserRole = data[0].Id.toString();
+        Request.Make("/Home/GetAllRoleNames/", "get").then(function (data) {
+            $scope.user.UserRole = data[0];
             $scope.roles = data;
         });
 
         
 
-        $scope.user = {
-            //Firstname: "Klas",
-            //Lastname: "Claywuald",
-            //SSN: "456456564",
-            //Phonenumber: "564654564",
-            //Email: "test2@test.com",
-            //Password: "Test@123",
-            //ConfirmPassword: "Test@123",
-            //UserRole: null
-        };
+        $scope.user = {};
+
+        //var DummyUser = {
+        //    Firstname: "Klas",
+        //    Lastname: "Claywuald",
+        //    SSN: "456456564",
+        //    Phonenumber: "564654564",
+        //    Email: "test2@test.com",
+        //    Password: "Test@123",
+        //    ConfirmPassword: "Test@123",
+        //    UserRole: "Teacher"
+        //};
 
         
         $scope.SendForm = sendForm;
