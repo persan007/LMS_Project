07b70bd16dc131b31a9d4168534f9d13c71c66d7
persan007 <per-ake.sandstrom@hotmail.se>
2017-01-10 08:50:15 +0100

    FileObjectUserModels igen

---
+++ LMS_Project/Models/FileObjectUserModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+
+
+namespace LMS_Project.Models
+{
+    public class FileObjectUserModels
+    {
+        [Key]
+        public string FileObjectUserID { get; set; }
+        
+        [ForeignKey("ApplicationUser")]
+        public virtual string UserID { get; set; }
+
+        [ForeignKey("FileObjectModels")]
+        public string FileObjectID { get; set; }
+
+        public bool Shared { get; set; }
+
+        public DateTime UploadedTime { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public FileObjectModels FileObjectModels { get; set; }
+
+        public FileObjectUserModels()
+        {
+        }
+    }
+}
